6efb5229fb7537d63e25aab537555a29a828e150
yelle-jansi <jansiyelle@gmail.com>
2019-11-20 15:26:56 +0530

    sharemarket model implemented

--- api/datawrkz/models.py
+++ api/datawrkz/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class ShareMarket(models.Model):
+    id = models.IntegerField(primary_key=True)
+    Date = models.DateField()
+    Open = models.FloatField(max_length=10)
+    High = models.FloatField(max_length=10)
+    Low = models.FloatField(max_length=10)
+    Close = models.FloatField(max_length=10)
+    Shares_Traded = models.IntegerField()
+    Turnover_Cr = models.FloatField(max_length=10)
+    
